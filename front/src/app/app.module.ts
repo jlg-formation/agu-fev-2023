8136e87105344f35e61ee3b37071f84ce76575bd
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-01 16:51:17 +0100

    dev: set default currency code to EUR

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { LOCALE_ID, NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { registerLocaleData } from '@angular/common';
 import localeFrBE from '@angular/common/locales/fr-BE';
@@ -10,13 +10,15 @@ import { HomeComponent } from './routes/home/home.component';
 import { LegalComponent } from './routes/legal/legal.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
-// the second parameter 'fr' is optional
 registerLocaleData(localeFrBE, 'fr-BE');
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, LegalComponent],
   imports: [BrowserModule, AppRoutingModule, LayoutModule, FontAwesomeModule],
-  providers: [{ provide: LOCALE_ID, useValue: 'fr-BE' }],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'fr-BE' },
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'EUR' },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
