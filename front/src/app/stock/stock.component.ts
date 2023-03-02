d73adcf4c5b71dcd847e1e2dd294b80dce438d4b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-02 11:05:35 +0100

    dev: added onDestroy

--- front/src/app/stock/stock.component.ts
+++ front/src/app/stock/stock.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import {
   faPlus,
   faRotateRight,
@@ -11,13 +11,16 @@ import { Article } from '../interfaces/article';
   templateUrl: './stock.component.html',
   styleUrls: ['./stock.component.scss'],
 })
-export class StockComponent {
-  faRotateRight = faRotateRight;
-  faPlus = faPlus;
-  faTrashCan = faTrashCan;
-
+export class StockComponent implements OnDestroy {
   articles: Article[] = [
     { name: 'Tournevis', price: 3.45, qty: 123 },
     { name: 'Pelle', price: 12, qty: 45 },
   ];
+  faPlus = faPlus;
+  faRotateRight = faRotateRight;
+  faTrashCan = faTrashCan;
+
+  ngOnDestroy(): void {
+    console.log('destroy stock');
+  }
 }
