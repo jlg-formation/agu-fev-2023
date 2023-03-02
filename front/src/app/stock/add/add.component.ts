4d719263457493158aab72239fb61f688f1b96b9
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-02 10:38:03 +0100

    dev: added form controle and check

--- front/src/app/stock/add/add.component.ts
+++ front/src/app/stock/add/add.component.ts
@@ -1,14 +1,35 @@
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
   styleUrls: ['./add.component.scss'],
 })
-export class AddComponent {
+export class AddComponent implements OnInit {
+  f = new FormGroup({
+    name: new FormControl('Truc', [
+      Validators.required,
+      Validators.maxLength(10),
+    ]),
+    price: new FormControl(0, [Validators.required, Validators.min(0)]),
+    qty: new FormControl(1, [Validators.required, Validators.min(0)]),
+  });
   faPlus = faPlus;
 
-  f = new FormGroup({});
+  ngOnInit(): void {
+    this.f.controls.qty.valueChanges.subscribe((value) => {
+      if (value === null) {
+        this.f.controls.qty.setValue(0, {
+          emitEvent: false,
+        });
+        return;
+      }
+      console.log('value: ', value);
+      this.f.controls.qty.setValue(Math.floor(value), {
+        emitEvent: false,
+      });
+    });
+  }
 }
