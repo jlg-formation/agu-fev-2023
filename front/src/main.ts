3c6ed6a253e6b4d67d1db4976f89ee1a363a8208
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-03 16:41:38 +0100

    dev: added url rewriting for spa

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,5 +1,12 @@
+import { isDevMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
+
+if (!isDevMode()) {
+  console.log = () => {
+    // yes it's empty
+  };
+}
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
