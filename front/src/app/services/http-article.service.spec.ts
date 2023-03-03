a62e53604e3006dadb5ff87eebaad0d3073b6290
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-03 13:57:01 +0100

    test: coverage ok for http article service

--- front/src/app/services/http-article.service.spec.ts
+++ front/src/app/services/http-article.service.spec.ts
@@ -3,6 +3,7 @@ import {
   HttpTestingController,
 } from '@angular/common/http/testing';
 import { fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { newArticle } from 'src/test/articles.data';
 
 import { HttpArticleService, url } from './http-article.service';
 
@@ -26,6 +27,73 @@ describe('HttpArticleService', () => {
     const req = httpTestingController.expectOne(url);
     expect(req.request.method).toEqual('GET');
     req.flush([], { status: 200, statusText: 'OK' });
+    tick(300);
+
+    expect(service).toBeTruthy();
+  }));
+
+  it('should be created with refresh in error', fakeAsync(() => {
+    const req = httpTestingController.expectOne(url);
+    expect(req.request.method).toEqual('GET');
+    req.flush('', { status: 404, statusText: 'Not found' });
+    expect(service).toBeTruthy();
+  }));
+
+  it('should call add', fakeAsync(() => {
+    let req = httpTestingController.expectOne(url);
+    expect(req.request.method).toEqual('GET');
+    req.flush([], { status: 200, statusText: 'OK' });
+    tick(300);
+
+    service.add(newArticle);
+    req = httpTestingController.expectOne(url);
+    expect(req.request.method).toEqual('POST');
+    req.flush('', { status: 201, statusText: 'Created' });
+    tick(300);
+
+    expect(service).toBeTruthy();
+  }));
+
+  it('should call add in error', fakeAsync(() => {
+    let req = httpTestingController.expectOne(url);
+    expect(req.request.method).toEqual('GET');
+    req.flush([], { status: 200, statusText: 'OK' });
+    tick(300);
+
+    service.add(newArticle);
+    req = httpTestingController.expectOne(url);
+    expect(req.request.method).toEqual('POST');
+    req.flush('', { status: 500, statusText: 'Internal Server Error' });
+    tick(300);
+
+    expect(service).toBeTruthy();
+  }));
+
+  it('should call delete', fakeAsync(() => {
+    let req = httpTestingController.expectOne(url);
+    expect(req.request.method).toEqual('GET');
+    req.flush([], { status: 200, statusText: 'OK' });
+    tick(300);
+
+    service.remove([]);
+    req = httpTestingController.expectOne(url);
+    expect(req.request.method).toEqual('DELETE');
+    req.flush('', { status: 204, statusText: 'No Content' });
+    tick(300);
+
+    expect(service).toBeTruthy();
+  }));
+
+  it('should call delete in error', fakeAsync(() => {
+    let req = httpTestingController.expectOne(url);
+    expect(req.request.method).toEqual('GET');
+    req.flush([], { status: 200, statusText: 'OK' });
+    tick(300);
+
+    service.remove([]);
+    req = httpTestingController.expectOne(url);
+    expect(req.request.method).toEqual('DELETE');
+    req.flush('', { status: 500, statusText: 'Internal Server Error' });
     tick(300);
 
     expect(service).toBeTruthy();
---
+++ front/src/test/articles.data.ts
@@ -0,0 +1,7 @@
+import { NewArticle } from 'src/app/interfaces/article';
+
+export const newArticle: NewArticle = {
+  name: 'Tournevis',
+  price: 4.5,
+  qty: 123,
+};
