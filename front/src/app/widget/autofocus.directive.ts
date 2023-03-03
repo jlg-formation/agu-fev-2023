40c497ce19a94e17cf293af5473b39d71a73daaf
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-03 14:33:02 +0100

    dev: add autofocus directive

--- front/src/app/widget/autofocus.directive.ts
+++ front/src/app/widget/autofocus.directive.ts
@@ -1,11 +1,22 @@
-import { Directive, OnDestroy } from '@angular/core';
+import { Directive, ElementRef, Input, OnDestroy, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appAutofocus]',
 })
-export class AutofocusDirective implements OnDestroy {
-  constructor() {
+export class AutofocusDirective implements OnDestroy, OnInit {
+  @Input('appAutofocus')
+  mode = '';
+
+  constructor(private readonly elt: ElementRef<HTMLInputElement>) {
     console.log('autofocus instantiated');
+    console.log('this.elt.nativeElement: ', this.elt.nativeElement);
+  }
+  ngOnInit(): void {
+    this.elt.nativeElement.focus();
+    console.log('this.mode: ', this.mode);
+    if (this.mode === 'selectall') {
+      this.elt.nativeElement.select();
+    }
   }
 
   ngOnDestroy(): void {
